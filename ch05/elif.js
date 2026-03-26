283816c3b741339dded4f04bc9195c2783d5a61d
ibingsu <biboy2908@gmail.com>
2026-03-26 12:30:28 +0900

    ch05

---
+++ ch05/elif.js
@@ -0,0 +1,27 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("점수를 입력하세요", function (score) {
+    // 조건 1. 점수가 90점 이상, 100점 이하인 경우 "장학대상"
+    // 조건 2. 점수가 60점 이상, 90점 미만인 경우 "합격"
+    // 조건 3. 점수가 0점 이상, 60점 미만인 경우 "불합격"
+    // 예외처리, 점수가 0점 미만, 100점 초과인 경우 "값이 유효하지 않습니다."
+    let result = null;
+
+    if (90 <= score && score <= 100) {
+        result = "장학대상";
+    } else if (60 <= score && score < 90) {
+        result = "합격";
+    } else if (0 <= score && score < 60) {
+        result = "불합격";
+    } else {
+        result = "값이 유효하지 않습니다.";
+    }
+
+    console.log(result);
+    rl.close();
+});
